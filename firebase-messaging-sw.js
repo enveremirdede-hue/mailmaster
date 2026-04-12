c6bad84f9591a0d3c6c0f8649a700fedebada1a4
enveremirdede-hue <enveremirdede@gmail.com>
2026-04-12 17:23:48 +0300

    Add Firebase messaging service worker script

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+// Senin Firebase Proje Ayarların
+firebase.initializeApp({
+    apiKey: "AIzaSyA1bzy3VaZJwUwlhybNt6yrVzurQsrcqLw",
+    projectId: "mailmaster-62785",
+    messagingSenderId: "324963953782",
+    appId: "1:324963953782:web:a2baa1cf64db51798bc357"
+});
+
+const messaging = firebase.messaging();
+
+// Site tamamen kapalıyken veya arka plandayken mesaj gelirse bu çalışır
+messaging.onBackgroundMessage(function(payload) {
+    console.log("Arka planda mesaj alındı: ", payload);
+    
+    const notificationTitle = payload.data ? payload.data.title : 'Mailmaster Pro';
+    const notificationOptions = {
+        body: payload.data ? payload.data.body : 'Yeni bir mesajın var!',
+        icon: 'https://cdn-icons-png.flaticon.com/512/733/733585.png'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
